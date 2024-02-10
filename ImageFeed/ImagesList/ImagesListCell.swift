64b3884b4db453238f9ad26ed96e30ef914537d8
Pavel Popov <Nazzeroost@yandex.ru>
2024-02-10 17:38:00 +0300

    no message

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+
 class ImagesListCell: UITableViewCell {
     
+    static let reuseIdentifier = "ImageListCell"
+    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
 }
