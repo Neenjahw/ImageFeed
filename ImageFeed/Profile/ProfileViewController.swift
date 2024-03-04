d631f1324c56dc40a0bce3cc4152022eaaaf602a
Pavel Popov <Nazzeroost@yandex.ru>
2024-03-04 18:47:37 +0300

    Спринт 9 на ревью

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Pavel Popov on 18.02.2024.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController {
+    
+}
---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Pavel Popov on 18.02.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet private var imageView: UIImageView!
+}
